11d6fc1abe5a5854f51efd548e7bcd80b24c037f
Bravin Thillainathan <bravint@gmail.com>
2021-10-18 16:51:34 +0100

    challenges completed, readme.md added

--- index.js
+++ index.js
@@ -20,12 +20,11 @@ For example, using figures in July 2008:
 */ 
 
 /* 
-Big Mac prices: Figures from the economist.com/big-mac-index
+Big Mac prices: Figures from the economist.com/big-mac-index (JULY 2021)
 
 us 5.65 (base)
 gb 3.49
 euro 4.27
-canada 6.77
 japan 390
 china 22.40
 russia 169
@@ -58,11 +57,17 @@ let bmiJapan = 0;
 let bmiChina = 0;
 let bmiRussia = 0;
 
-bmiGb = ((erGb - (bmUsa/bmGb)) / (bmUsa/bmGb)) * 100;
+bmiGb = ((erGb - (bmUsa/bmGb)) / (bmUsa/bmGb)) *100;
 bmiEuro = ((erEuro - (bmUsa/bmEuro)) / (bmUsa/bmEuro)) * 100;
 bmiJapan = ((erJapan - (bmUsa/bmJapan)) / (bmUsa/bmJapan)) * 100;
 bmiChina =  ((erChina - (bmUsa/bmChina)) / (bmUsa/bmChina)) * 100;
 bmiRussia =  ((erRussia - (bmUsa/bmRussia)) / (bmUsa/bmRussia)) * 100;
+
+bmiGb = bmiGb.toFixed(2)
+bmiEuro = bmiEuro.toFixed(2)
+bmiJapan = bmiJapan.toFixed(2)
+bmiChina = bmiChina.toFixed(2)
+bmiRussia = bmiRussia.toFixed(2)
 
 console.log (bmiGb)
 /* -15.38% */
@@ -74,3 +79,50 @@ console.log (bmiChina)
 /* -36.57% */
 console.log (bmiRussia)
 /* -58.12% */
+
+/*CHALLENGE 1*/
+
+let bmiUsa = 0;
+
+console.log (bmiGb > bmiUsa);
+console.log (bmiEuro > bmiUsa);
+console.log (bmiJapan > bmiUsa);
+console.log (bmiChina > bmiUsa);
+console.log (bmiRussia > bmiUsa);
+
+/* 
+True = means the US Dollar is undervalued vs the compared currency 
+False = means the compared currency is undervalued vs the US Dollar
+*/
+
+/*CHALLENGE 2*/
+
+if (bmiGb > bmiUsa) {
+    console.log('The US Dollar is undervalued vs the Pound Sterling by '+ bmiGb +'%');
+} else {
+    console.log ('The Pound Sterling is undervalued vs the US Dollar  by '+ (-bmiGb) +'%');
+}
+
+if (bmiEuro > bmiUsa) {
+    console.log('The US Dollar is undervalued vs the Euro by '+ bmiEuro +'%');
+} else {
+    console.log ('The Euro is undervalued vs the US Dollar  by '+ (-bmiEuro) +'%');
+}
+
+if (bmiJapan > bmiUsa) {
+    console.log('The US Dollar is undervalued vs the Japanese Yen by '+ bmiJapan +'%');
+} else {
+    console.log ('The Japanese Yen is undervalued vs the US Dollar  by '+ (-bmiJapan) +'%');
+}
+
+if (bmiChina > bmiUsa) {
+    console.log('The US Dollar is undervalued vs the Chinese Yuan by '+ bmiChina +'%');
+} else {
+    console.log ('The Chinese Yuan is undervalued vs the US Dollar  by '+ (-bmiChina) +'%');
+}
+
+if (bmiRussia > bmiUsa) {
+    console.log('The US Dollar is undervalued vs the Russian Rouble by '+ bmiRussia);
+} else {
+    console.log ('The Russian Rouble is undervalued vs the US Dollar  by '+ (-bmiRussia) +'%');
+}
